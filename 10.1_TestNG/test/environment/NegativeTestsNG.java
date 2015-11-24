4c95a81812c37468dfe8bc39d12f03d119486a7a
Andrey_Vyalov <Andrey_Vyalov@EPRURYAW0075.moscow.epam.com>
2015-11-24 14:24:48 +0300

    TestNG AT task

---
+++ 10.1_TestNG/test/environment/NegativeTestsNG.java
@@ -0,0 +1,30 @@
+package environment;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import epam.saratov.homeWork.testng.objects.GeometricObjects;
+import epam.saratov.homeWork.testng.objects.GeometricObjects.Rectangle;
+
+public class NegativeTestsNG {
+	private double sideOne = 5;
+	private double sideTwo = 10;
+	private Rectangle rectangle;
+
+	@BeforeTest
+	public void testPreparation() {
+		this.rectangle = new GeometricObjects().getRectangle(sideOne, sideTwo);
+	}
+
+	@AfterTest
+	public void testFinalisation() {
+		this.rectangle = null;
+	}
+
+	@Test
+	public void testRectangleIsNotQuadrate() {
+		Assert.assertFalse(this.rectangle.isQuadrate());
+	}
+}
