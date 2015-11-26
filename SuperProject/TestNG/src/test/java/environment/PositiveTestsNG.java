7ebc9c4c430ad07542c2a7ee6cdf5953cd32d269
Andrey_Vyalov <Andrey_Vyalov@EPRURYAW0075.moscow.epam.com>
2015-11-26 18:43:12 +0300

    mavvin

--- SuperProject/TestNG/src/test/java/environment/PositiveTestsNG.java
+++ SuperProject/TestNG/src/test/java/environment/PositiveTestsNG.java
@@ -99,6 +99,6 @@ public class PositiveTestsNG {
 	@Test(groups = "rectangle", dependsOnMethods = { "testRectangleGetSquare" })
 	public void testRectangleIsQuadrate() {
 		this.rectangle = new GeometricObjects().getRectangle(sideOne, sideTwo);
-		Assert.assertTrue(this.rectangle.isQuadrate());
+		Assert.assertTrue(this.rectangle.isQuadrate() == (sideOne == sideTwo));
 	}
 }
