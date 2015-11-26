96db8a9d7661fd8a29582e377274b21e250a995b
Andrey_Vyalov <Andrey_Vyalov@EPRURYAW0075.moscow.epam.com>
2015-11-26 16:19:30 +0300

    werwer

---
+++ SuperProject/JUnit/src/test/java/num/NumGuessTest.java
@@ -0,0 +1,65 @@
+package num;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NumGuessTest {
+	NumberGuessBean numberGuessInstance;
+
+	@Before
+	public void setUp() {
+		this.numberGuessInstance = new NumberGuessBean();
+	}
+
+	@After
+	public void tearDown() {
+		this.numberGuessInstance = null;
+	}
+
+	@Test
+	public void testNewInstanceFieldsDefaultness() {
+		assertEquals(false, this.numberGuessInstance.getSuccess());
+		assertEquals(0, this.numberGuessInstance.getNumGuesses());
+		assertTrue(this.numberGuessInstance.answer >= 1
+				&& this.numberGuessInstance.answer <= 100);
+		assertEquals(null, this.numberGuessInstance.hint);
+	}
+
+	@Test
+	public void testIfLowerThenHintIsHigher() {
+		this.numberGuessInstance.answer = 2;
+		this.numberGuessInstance.setGuess("1");
+		assertEquals("higher", this.numberGuessInstance.getHint());
+	}
+
+	@Test
+	public void testIfHigherThenHintIsLower() {
+		this.numberGuessInstance.answer = 1;
+		this.numberGuessInstance.setGuess("2");
+		assertEquals("lower", this.numberGuessInstance.getHint());
+	}
+
+	@Test
+	public void testOutOfRange() {
+		this.numberGuessInstance.setGuess("101");
+		assertEquals("a number next time", this.numberGuessInstance.getHint());
+		this.numberGuessInstance.setGuess("-1");
+		assertEquals("a number next time", this.numberGuessInstance.getHint());
+	}
+
+	@Test
+	public void testReset() {
+		this.numberGuessInstance.setGuess("50");
+		this.numberGuessInstance.answer = 50;
+		this.numberGuessInstance.reset();
+
+		assertEquals(false, this.numberGuessInstance.getSuccess());
+		assertEquals(0, this.numberGuessInstance.getNumGuesses());
+		assertTrue(this.numberGuessInstance.answer >= 1
+				&& this.numberGuessInstance.answer <= 100);
+	}
+}
